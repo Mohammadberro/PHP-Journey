ee931abdbd5128f5db006b0d33000f5bcbff35a7
Mohammadberro <ikerberro1@gmail.com>
2024-03-12 08:42:37 +0200

    Operation evaluation

---
+++ operationEvaluation.php
@@ -0,0 +1,49 @@
+<?php
+
+function findOperator($string) {
+    $operators = array("+", "-", "/", "*");
+
+    for ($i = 0; $i < strlen($string); $i++) {
+        if (in_array($string[$i], $operators)) {
+            $operator = $string[$i];
+            $firstOperand = '';
+            for ($j = $i - 1; $j >= 0; $j--) {
+                $firstOperand = $string[$j] . $firstOperand;
+            }
+
+            $secondOperand = '';
+            for ($n = $i + 1; $n < strlen($string); $n++) {
+                $secondOperand = $secondOperand . $string[$n];
+            }
+
+            $firstOperand = floatval($firstOperand);
+            $secondOperand = floatval($secondOperand);
+
+            switch ($operator) {
+                case '+':
+                    $result = $firstOperand + $secondOperand;
+                    echo "Result: $result\n";
+                    break;
+                case '-':
+                    $result = $firstOperand - $secondOperand;
+                    echo "Result: $result\n";
+                    break;
+                case '*':
+                    $result = $firstOperand * $secondOperand;
+                    echo "Result: $result\n";
+                    break;
+                case '/':
+                    if ($secondOperand != 0) {
+                        $result = $firstOperand / $secondOperand;
+                        echo "Result: $result\n";
+                    } else {
+                        echo "Error: Division by zero\n";
+                    }
+                    break;
+                default:
+                    echo "Error: Invalid operator\n";
+            }
+        }
+    }
+}
+
